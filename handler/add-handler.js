16b439d1ddd50ef0b67a32a0e96f16a4205cc047
oguzkarademir <oguzkarademir26@gmail.com>
2020-10-26 15:21:36 +0100

    complete files

---
+++ handler/add-handler.js
@@ -0,0 +1,30 @@
+function addLogic (input1, input2) {  
+
+    if (typeof input1 !== 'number') {throw new TypeError('input1');}
+    if (typeof input2 !== 'number') {throw new TypeError('input2');}     
+
+    const result = input1 + input2;
+
+    if(typeof result !== 'number') {throw new TypeError('result');}
+
+    return result;
+}
+
+function addHandler() {
+    debugger;
+
+    const userInput1 = document.getElementById("first-input").value;
+    const userInput2 = document.getElementById("second-input").value;
+    
+    let lastResult;
+
+    if((isNaN(userInput1) ===true) || (isNaN(userInput2) === true)){
+        alert('Please enter a number');
+        return;
+    } else {
+
+        lastResult = addLogic(Number(userInput1), Number(userInput2));
+    }
+    
+    alert(lastResult);
+}
